84f82a56ef86e2158fd9c514cd55806bf3e5cfba
Rishiraj <rishirajnatj@gmail.com>
2026-05-02 23:20:28 +0530

    Updates in backend

--- backend/src/prisma/client.js
+++ backend/src/prisma/client.js
@@ -2,7 +2,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env"), override: true });
 
 const clientOptions = process.env.DATABASE_URL
   ? {
